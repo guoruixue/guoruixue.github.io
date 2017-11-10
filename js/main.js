b4216c4c7c3ed1e75bf9172dabc3e6b4b9e4acb6
2524857256@qq.com <2524857256@qq.com>
2017-11-10 22:06:51 +0800

    Site updated: 2017-11-10 22:06:50

--- js/main.js
+++ js/main.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
 
-  if (window.location.pathname != "/") {       // if hexo in subdir of site, should change this line
-    $('.panel-cover').addClass('panel-cover--collapsed');
-  }
+  if (window.location.pathname != "/huno/") {
+  $('.panel-cover').addClass('panel-cover--collapsed');
+ }
 
   $('.btn-mobile-menu').click(function() {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
